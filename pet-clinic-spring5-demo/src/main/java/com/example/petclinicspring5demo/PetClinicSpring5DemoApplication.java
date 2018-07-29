a260e839270d7d8d8f6d8895ca027765a84b0e8a
michaelmenard <mlmmayhem@gmail.com>
2018-07-29 12:16:49 -0600

    git init

---
+++ pet-clinic-spring5-demo/src/main/java/com/example/petclinicspring5demo/PetClinicSpring5DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.petclinicspring5demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpring5DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpring5DemoApplication.class, args);
+    }
+}
